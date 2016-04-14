bebf15c3564c6bb4210a0067ee095b842b20c5ad
root <root@vps76571.vps.ovh.ca>
2016-04-14 00:58:06 -0400

    Update to rproxy.go

--- rproxy.go
+++ rproxy.go
@@ -2,12 +2,21 @@ package main
 
 import (
   "net/http"
+  "github.com/gorilla/mux"
+  "strings"
 )
 
-type sites struct {
-  s1, s2 string
+func HandleIt(w http.ResponseWriter, r *http.Request) {
+  if strings.Contains(r.Host, "rebirtharmitage.com"){
+    http.Redirect(w, r, "http://www.rebirtharmitage.com:8080", 302)
+  }else if strings.Contains(r.Host, "internetatlas.co"){
+    http.Redirect(w, r, "http://www.internetatlas.co:8081", 302)
+  }
 }
 
+var router = mux.NewRouter()
+
 func main(){
-  
+  http.HandleFunc("/", HandleIt)
+	http.ListenAndServe(":80", nil)
 }
