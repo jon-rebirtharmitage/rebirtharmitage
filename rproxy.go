38d2f10ed82560d9488844a95762294eb8495541
root <root@vps76571.vps.ovh.ca>
2017-11-17 11:14:21 -0500

    Current Working Versions of RProxy and Website

--- rproxy.go
+++ rproxy.go
@@ -8,7 +8,7 @@ import (
 
 func HandleIt(w http.ResponseWriter, r *http.Request) {
   if strings.Contains(r.Host, "wiki.rebirtharmitage.com"){
-    http.Redirect(w, r, "http://wiki.rebirtharmitage.com:8085", 302)
+    http.Redirect(w, r, "https://wiki.rebirtharmitage.com:8085", 302)
   }else if strings.Contains(r.Host, "internetatlas.co"){
     http.Redirect(w, r, "http://www.internetatlas.co:8081", 302)
   }else if strings.Contains(r.Host, "tattoos"){
